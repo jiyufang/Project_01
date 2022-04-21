57b399a49a4f99aec0daccefb2394650e829008e
岳岳 <1060372360@qq.com>
2022-04-21 22:41:22 +0800

    编程式导航

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="home-container">
     <h3>Home 组件</h3>
+    <hr />
+    <button @click="gotoLoki">通过 push 跳转到“洛基”页面</button>
+    <button @click="gotoLoki2">通过 replace 跳转到“洛基”页面</button>
   </div>
 </template>
 
 <script>
 export default {
-
+  name: 'Home',
+  methods: {
+    gotoLoki() {
+      this.$router.push('/movie/1')
+    },
+    gotoLoki2() {
+      this.$router.replace('/movie/2')
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
-  .home-container {
-    background-color: pink;
-    min-height: 200px;
-  }
+.home-container {
+  background-color: pink;
+  min-height: 200px;
+}
 </style>
--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -4,15 +4,25 @@
     <!-- this.$router 是路由的 “导航对象” -->
     <h3>Movie 组件 ------{{ $route.params.mid }}-----{{ mid }}</h3>
     <button @click="showThis">打印</button>
+    <button @click="goBack">go 后退</button>
+    <button @click="$router.back()">goback 后退</button>
+    <button @click="$router.forward()">goback 前进</button>
   </div>
 </template>
 
 <script>
 export default {
-  props:['mid'],
+  name: 'Movie',
+  props: ['mid'],
   methods: {
     showThis() {
       console.log(this)
+    },
+    goBack() {
+      // 后退到之前的组件页面
+      // go(-1) 表示后退一层
+      // 如果后退的层数超过上限，则原地不动
+      this.$router.go(-1)
     }
   }
 }
